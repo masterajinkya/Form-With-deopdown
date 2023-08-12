2c0c3a80c330ac8fd9a1c6fe1ea3140489305f90
MasterAjinkya <137719809+masterajinkya@users.noreply.github.com>
2023-08-12 11:18:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
 import Form from "./Form"
+import Form1 from "./Form1"
 
 function App() {
   return (
     < >
 <Form/>
+<Form1/>
     </>
   );
 }
